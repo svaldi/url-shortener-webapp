1192d01e32e53687f93a26cd2b4d1cc76e8c97eb
Luis Valdivia <svaldi.lsvl@gmail.com>
2017-12-13 12:23:50 -0600

    adding provider to handle store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './redux/configureStore';
+
 import './styles/index.css';
 import App from './components/app/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
