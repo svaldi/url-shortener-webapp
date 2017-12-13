6809426443b3ee130991ba418fd351fd9ea5d67f
Luis Valdivia <svaldi.lsvl@gmail.com>
2017-12-13 11:25:59 -0600

    adding enzyme & testing helpers

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('App component', () => {
+  it('renders without crashing', () => {
+    shallow(<App />);
+  });
+
+  it('renders welcome message', () => {
+    const wrapper = shallow(<App />);
+    const welcome = <h1 className="App-title">Welcome to React</h1>;
+
+    expect(wrapper.contains(welcome)).toEqual(true);
+  });
 });
