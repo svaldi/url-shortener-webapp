cf89f91fd2255f970366edf015818828c7d4c998
Luis Valdivia <svaldi.lsvl@gmail.com>
2017-12-14 13:05:11 -0600

    form validation

--- src/containers/url-shortener/UrlForm.js
+++ src/containers/url-shortener/UrlForm.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import validate from '../../helpers/generateValidation';
+
+const renderField = ({
+  input,
+  label,
+  placeholder,
+  type,
+  meta: { touched, error }
+}) => (
+  <div>
+    <label>{label}</label>
+    <div>
+      <input
+        {...input}
+        placeholder={placeholder}
+        type={type}
+        className="get-short-url"
+      />
+      {touched && (error && <span className="error">{error}</span>)}
+    </div>
+  </div>
+);
 
 let UrlForm = props => {
   const { handleSubmit } = props;
   return (
     <form onSubmit={handleSubmit}>
       <div>
-        <label htmlFor="longUrl">Long URL: </label>
-        <Field name="longUrl" component="input" type="text" />
+        <Field
+          name="longUrl"
+          type="text"
+          component={renderField}
+          label="Get a short url"
+          placeholder="http://www.example.com/hello/mylink"
+        />
       </div>
       <button type="submit">Submit</button>
     </form>
@@ -15,7 +42,8 @@ let UrlForm = props => {
 };
 
 UrlForm = reduxForm({
-  form: 'generateShortUrlForm'
+  form: 'generateShortUrlForm',
+  validate
 })(UrlForm);
 
 export default UrlForm;
