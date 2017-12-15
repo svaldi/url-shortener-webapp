b2330b0e28ee50102f10532e9cf939ff3f1572da
Luis Valdivia <svaldi.lsvl@gmail.com>
2017-12-14 18:26:54 -0600

    adding error handler when creating short url

--- src/redux/reducers/url-shortener/url-shortener-reducer.js
+++ src/redux/reducers/url-shortener/url-shortener-reducer.js
@@ -1,28 +1,33 @@
 import {
   REQUEST_URLS,
   RECEIVE_URLS,
-  CREATE_URL
+  CREATE_URL_SUCCESS,
+  CREATE_URL_FAILED
 } from './url-shortener-actionTypes';
 
 const INITIAL_STATE = {
   isFetching: false,
   urls: [],
-  activeURL: null
+  activeURL: null,
+  error: null
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case REQUEST_URLS:
-      return { ...state, isFetching: true };
+      return { ...state, isFetching: true, error: null };
     case RECEIVE_URLS:
       return { ...state, urls: action.payload, isFetching: false };
-    case CREATE_URL:
+    case CREATE_URL_SUCCESS:
       const url = { _id: action.payload.hash, ...action.payload }; // temporal fix for missing _id
       return {
         ...state,
         urls: [url, ...state.urls],
-        activeURL: action.payload
+        activeURL: action.payload,
+        error: null
       };
+    case CREATE_URL_FAILED:
+      return { ...state, error: action.error };
     default:
       return state;
   }
